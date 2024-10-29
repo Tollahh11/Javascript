eea2ac218ba491e01f38f54346c50163b5349180
Tollahh11 <omotolaboluwatife04@gmail.com>
2024-10-29 18:35:14 +0100

    function

---
+++ Week one/about.js
@@ -0,0 +1,6 @@
+document.getElementById("head").innerHTML = "About Myself"
+document.getElementById("pass").innerHTML = "My name is Adekoya Boluwatife Omotola. I am a recent graduate of Olabisi Onabanjo University. I have a Bsc in Microbiology. I have a great passion for tech skills. The OSF has really given me a great opportunity to explore the horizon of Tech with the help of Equitech Education Technology <br>"
+function addition(){
+    document.write("I am just learning")
+}
+addition()
